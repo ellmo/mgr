85006b931c95a54fc4adfe2bd90bcc48fe4a729a
ellmo <ellmunzai@gmail.com>
2011-10-29 16:04:05 +0200

    User Mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,23 @@
+class UserMailer < ActionMailer::Base
+  include Devise::Mailers::Helpers
+  default :from => "ellmunzai@gmail.com"
+  
+  def confirmation_instructions(record)
+    devise_mail(record, :confirmation_instructions)
+  end
+
+    # send password reset instructions
+  def reset_password_instructions(user)
+    @resource = user
+    mail(:to => @resource.email,
+      :subject => "Reset password instructions",
+      :tag => 'password-reset',
+      :content_type => "text/html") do |format|
+        format.html { render "devise/mailer/reset_password_instructions" }
+    end
+  end
+
+  def unlock_instructions(record)
+    devise_mail(record, :unlock_instructions)
+  end
+end
